1591f148ee55752920167784f5147756cbb96455
raquelvvarona <raquelvarona@usf.edu>
2020-07-04 15:20:57 -0400

    Commit with Form

--- MVC_Start/Controllers/HomeController.cs
+++ MVC_Start/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using MVC_Start.Models;
 
 namespace MVC_Start.Controllers
 {
+<<<<<<< HEAD
   public class HomeController : Controller
   {
     public IActionResult Index()
@@ -44,4 +45,46 @@ namespace MVC_Start.Controllers
       return View(Model);
     }
   }
+=======
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IndexWithLayout()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            GuestContact contact = new GuestContact();
+
+            contact.Name = "Raquel Varona";
+            contact.Email = "raquelvarona@usf.edu";
+            contact.Phone = "813-431-5727";
+
+            return View(contact);
+
+        }
+
+        public ViewResult DemoChart()
+        {
+            string[] ChartLabels = new string[] { "Africa", "Asia", "Europe", "Latin America", "North America" };
+            int[] ChartData = new int[] { 2478, 5267, 734, 784, 433 };
+
+            ChartModel Model = new ChartModel
+            {
+                ChartType = "bar",
+                Labels = String.Join(",", ChartLabels.Select(d => "'" + d + "'")),
+                Data = String.Join(",", ChartData.Select(d => d)),
+                Title = "Predicted world population (millions) in 2050"
+            };
+
+            return View(Model);
+        }
+    }
+>>>>>>> 3ad7330... With Form
 }
